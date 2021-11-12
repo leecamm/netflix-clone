caad779c1a3bfa45382b6feb5588cfbdc0151a0c
Le <hieu.le@accenture.com>
2021-11-12 20:42:15 +0100

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyACe1Y-RVjsK0IP0ylrPQ4R-4YcBy42y3M",
+    authDomain: "netflix-clone-1039b.firebaseapp.com",
+    projectId: "netflix-clone-1039b",
+    storageBucket: "netflix-clone-1039b.appspot.com",
+    messagingSenderId: "228916473239",
+    appId: "1:228916473239:web:a1d01189d00bf657bda8d2"
+  };
